53633e3e753e9c5be17c401b49b7d01badd5dcbb
Vladyslav94 <grigoren_vlad2009@yahoo.com>
2020-03-12 13:43:19 +0200

    App already can get file and put it to the archive.

--- ZipFileManager.java
+++ ZipFileManager.java
@@ -1,15 +1,31 @@
 package com.javarush.task.task31.task3110;
 
+import java.io.File;
+import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class ZipFileManager {
     private Path zipFile;
 
-    public ZipFileManager(Path zipFile){
+    public ZipFileManager(Path zipFile) {
         this.zipFile = zipFile;
     }
 
-    public void createZip(Path source) throws Exception{
+    public void createZip(Path source) throws Exception {
+        try (ZipOutputStream zipOutputStream = new ZipOutputStream(Files.newOutputStream(zipFile));
+             InputStream inputStream = Files.newInputStream(source)) {
+            ZipEntry zipEntry = new ZipEntry(source.getFileName().toString());
+            zipOutputStream.putNextEntry(zipEntry);
+            int len;
+            byte[] buffer = new byte[inputStream.available()];
+            while ((len = inputStream.read(buffer)) > 0) {
+                zipOutputStream.write(buffer, 0, len);
+
+            }
+        }
 
     }
 }
